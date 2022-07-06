49193084c8b6bba751b324b339b0d70109f01d84
JanDuchnowski <tomex2001@gmail.com>
2022-07-06 22:01:41 +0200

    init

---
+++ flappy_tree/lib/bird.dart
@@ -0,0 +1,10 @@
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+class Player extends PositionComponent {
+  static final _paint = Paint()..color = Colors.white;
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRect(size.toRect(), _paint);
+  }
+}

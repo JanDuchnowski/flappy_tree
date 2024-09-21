823cd9765bf8b627c0f099e3f35d071ca429395e
Jan Duchnowski <jan.duchnowski@autonomik.pl>
2024-09-21 16:51:42 +0200

    Updated dependencies and added smooth jumping animation]

--- flappy_tree/lib/tree.dart
+++ flappy_tree/lib/tree.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flame_audio/flame_audio.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_application_1/game.dart';
 import 'package:flutter_application_1/game_state.dart';
 import 'package:flutter_application_1/sounds.dart';
@@ -29,15 +31,19 @@ class Tree extends SpriteComponent
   void jump() {
     if (!GameState().wasHit) {
       Sounds.jumpSound();
-      position.add(Vector2(0, -30));
+      // position.add(Vector2(0, -30));
+      add(MoveByEffect(
+        Vector2(0, -30),
+        EffectController(duration: 0.2, curve: Curves.decelerate),
+      ));
     }
   }
 
   @override
   void update(double dt) {
+    super.update(dt);
     if (!GameState().wasHit && GameState().hasGameStarted) {
       position = Vector2(position.x, position.y + 1.0);
-      super.update(dt);
     }
   }
 }
